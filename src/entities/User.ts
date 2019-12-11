c0d430584c0c38515dc1dec8dbfe56611fce9a53
Jane Lee <Yesdoing13@gmail.com>
2019-12-11 21:17:38 +0900

    feat: add Entity about Verification & Place

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import { IsEmail } from "class-validator";
 import {
   BaseEntity,
@@ -5,8 +6,12 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate
 } from "typeorm";
+
+const BCRYPT_ROUNDS = 10;
 
 @Entity()
 class User extends BaseEntity {
@@ -67,6 +72,24 @@ class User extends BaseEntity {
 
   @UpdateDateColumn()
   updatedAt: string;
+
+  public comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async savePassword(): Promise<void> {
+    if(this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, BCRYPT_ROUNDS);
+  }
+
 }
 
 export default User;
