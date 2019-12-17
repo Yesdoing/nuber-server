2b386e6315e020f938ed4f63f7a2da95ae237139
Jane Lee <Yesdoing13@gmail.com>
2019-12-17 23:17:19 +0900

    feat: add EmailSignIn graphql and resolvers

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -0,0 +1,49 @@
+import { Resolvers } from "src/types/resolvers";
+import { EmailSignInMutationArgs, EmailSignInResponse } from "src/types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (
+      _,
+      args: EmailSignInMutationArgs
+    ): Promise<EmailSignInResponse> => {
+      const { email, password } = args;
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: "No User found with that Email",
+            token: null
+          }
+        }
+
+        const checkPassword = user.comparePassword(password);
+
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: "Comming soon"
+          }
+        } else {
+          return {
+            ok: false,
+            error: "Wrong Password",
+            token: null
+          }
+        }
+
+      } catch (error) {
+        return {
+          ok: false,
+          error: error,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
